2c5371f9722b1876cc4837192aa1269699b54ff9
Steven Burnett <stevendburnett@gmail.com>
2017-08-13 16:18:06 -0600

    version 1

--- SwipeCards.js
+++ SwipeCards.js
@@ -5,11 +5,19 @@ import React, { Component } from 'react';
 import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 
 import SwipeCards from 'react-native-swipe-cards';
+const colors = [
+ 'red',
+  'purple',
+  'green',
+  'blue',
+  'cyan',
+  'orange',
+]
 
 let Card = React.createClass({
   render() {
     return (
-      <TouchableOpacity onPress={_ => this.props.onTouchCallback({text: this.props.text})}>
+      <TouchableOpacity onPress={_ => this.props.onTouchCallback({text: this.props.text, uri: this.props.uri})}>
         <View style={[styles.card, {backgroundColor: this.props.backgroundColor}]}>
           <Text>{this.props.text}</Text>
         </View>
@@ -31,14 +39,138 @@ class NoMoreCards extends Component {
     )
   }
 }
-
 const Cards = [
-  {text: 'Tomato', backgroundColor: 'red'},
-  {text: 'Aubergine', backgroundColor: 'purple'},
-  {text: 'Courgette', backgroundColor: 'green'},
-  {text: 'Blueberry', backgroundColor: 'blue'},
-  {text: 'Umm...', backgroundColor: 'cyan'},
-  {text: 'orange', backgroundColor: 'orange'},
+  {text: "Good afternoon", uri: "goodafternoon.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Good morning", uri: "goodmorning.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Goodbye", uri: "bye.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Goodnight", uri: "goodnight.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Hello", uri: "hello.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "How are you?", uri: "howareyou.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "How are you? (2)", uri: "howareyou2.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "My name is Deborah (name sign).", uri: "mynamedeborah.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Nice to meet you.", uri: "nicetomeetyou.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Thank you.", uri: "thankyou.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Thank you. (2)", uri: "thank-you.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "What is your name?", uri: "whatyourname.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "You’re welcome.", uri: "yourewelcome.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "ASL Alphabet (A-Z)", uri: "lesson-videos/Unit1_Alphabet.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "BOY", uri: "boy.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "DEAF", uri: "deaf.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "GIRL", uri: "girl.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "HEARING", uri: "hearing.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "HOW", uri: "how.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "HOW (2)", uri: "how2.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MAN", uri: "man.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MAN (2)", uri: "man2.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "PLEASE", uri: "please.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "STUDENT", uri: "student.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "STUDENT (2)", uri: "student2.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "TEACHER", uri: "teacher.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WHAT", uri: "what.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WHEN", uri: "when.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WHERE", uri: "where.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WHO", uri: "who.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WHY", uri: "why.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WOMAN", uri: "woman.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WOMAN (2)", uri: "woman2.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Can you (please) sign slower?", uri: "pleasesignslower.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Can you (please) sign that again?", uri: "pleasesignagain.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Can you fingerspell that again?", uri: "fingerspellagain.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Excuse me", uri: "excuseme.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "I don’t understand.", uri: "idontunderstand.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "I’ll be right back.", uri: "ibrb.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Sorry.", uri: "sorry.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "There is no sign for that, you need to fingerspell it.", uri: "nosignforthatmustfingerspell.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "What did you/they say?", uri: "whatsay.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "What does that mean?", uri: "whatdoesthatmean.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Write it please.", uri: "pleasewrite.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "AGAIN", uri: "again.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "APPLE", uri: "apple-2.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "APPLE (2)", uri: "apple.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "BALL", uri: "ball.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "BOOK", uri: "book.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "BREAD", uri: "bread.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "CANDY", uri: "candy.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "COKE (Coca Cola)", uri: "coke-2.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "DRAW", uri: "draw.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "DRINK", uri: "drink.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "EAT", uri: "eat.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "FOOD", uri: "food.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "GRAPES", uri: "grapes.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "ICE CREAM", uri: "ice-cream.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "KEY", uri: "key.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "LOCK", uri: "lock.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MILK", uri: "milk.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MORE", uri: "more.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Numbers 1-10", uri: "lesson-videos/Unit2_Numbers_1-10.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "PAPER", uri: "paper.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "PIZZA", uri: "pizza.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "PIZZA (2)", uri: "pizza-2.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SENTENCE", uri: "sentence.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SIGN", uri: "sign.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SODA-POP", uri: "soda-pop.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WANT", uri: "want.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WANT (2)", uri: "want-2.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WATER", uri: "water.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WRITE", uri: "write.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Are you a student?", uri: "areyouastudent.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Are you deaf?", uri: "areyoudeaf.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Are you learning sign language?", uri: "youlearnsign.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "No, I’m not deaf, I’m hearing.", uri: "noimnotdeafimhearing.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "What is your teacher’s name?", uri: "whatsyourteachersname.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Where are you learning sign language?", uri: "whereyoulearnsign.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Who is your teacher?", uri: "whoisyourteacher.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "Why are you learning sign language?", uri: "whyyoulearnsign.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "You sign very fluently.", uri: "yousignveryfluent.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "ALGEBRA", uri: "algebra.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "ALWAYS", uri: "always.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "ART", uri: "art.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "ASL", uri: "asl.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "BAND", uri: "band.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "BIOLOGY", uri: "biology.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "CHEMISTRY", uri: "chemistry.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "CHOIR", uri: "choir.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "COLLEGE", uri: "college.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "COOK", uri: "cook.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "DRAMA", uri: "drama.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "ELEMENTARY", uri: "elementary-school.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "ENGLISH", uri: "english.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "GET-UP", uri: "get-up.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "HEALTH", uri: "health.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "HIGH-SCHOOL", uri: "highschool.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "HISTORY", uri: "history.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "JUNIOR-HIGH-SCHOOL", uri: "junior-high-school.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "KNOW", uri: "know.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "LEARN", uri: "learn.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MAJOR (in college)", uri: "major.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MARTIAL ARTS", uri: "martial-arts.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MATH", uri: "math.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MIDDLE SCHOOL", uri: "middle-school.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MOON", uri: "moon.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "MOST", uri: "most.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "NEED", uri: "need.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "NEVER", uri: "never.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "NO", uri: "no.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "PHOTOGRAPHY", uri: "photography.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "PHYSICS", uri: "physics.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "PLAY", uri: "play.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "POETRY", uri: "poetry.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "PSYCHOLOGY", uri: "psychology.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SCHOOL", uri: "school.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SCIENCE", uri: "science.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SEASON", uri: "season.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SIGN LANGUAGE", uri: "sign-language.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SKULPTING/CERAMICS", uri: "skulpting-ceramics.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SLEEP", uri: "sleep.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SPORTS", uri: "sports.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "SUN", uri: "sun.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "THIRSTY", uri: "thirsty.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "UNIVERSITY", uri: "university.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WAKE-UP", uri: "wake-up.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WARM", uri: "warm.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WEATHER", uri: "weather.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "WORK", uri: "work.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
+  {text: "YES", uri: "yes.mp4", backgroundColor: colors[Math.floor(Math.random() * colors.length)]},
 ]
 
 export default class CustomSwipeCards extends React.Component {
@@ -47,24 +179,47 @@ export default class CustomSwipeCards extends React.Component {
   }
   constructor(props) {
     super(props)
-    this.state = {cards: Cards}
+    this.state = {cards: this.shuffle(Cards)}
   }
+
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
   handleYup = (card) => {
-    console.log(`Yup for ${card.text}`)
+    let newCards = this.state.cards.filter(function( obj ) {
+        return obj.text !== card.text;
+    });
+    this.setState({cards: newCards})
   }
   handleNope = (card) => {
-    const Round2Cards = [
-      {text: 'Aubergine', backgroundColor: 'purple'},
-      {text: 'Courgette', backgroundColor: 'green'},
-      {text: 'Blueberry', backgroundColor: 'blue'},
-      {text: 'Umm...', backgroundColor: 'cyan'},
-      {text: 'orange', backgroundColor: 'orange'},
-    ]
-    this.setState({cards: Round2Cards})
+    let nopedObject = this.state.cards.filter(function( obj ) {
+        return obj.text === card.text;
+    })[0];
+    let newCards = this.state.cards.filter(function( obj ) {
+        return obj.text !== card.text;
+    });
+    newCards.push(nopedObject)
+    this.setState({cards: newCards})
   }
   handleCardTouch = (card) => {
     const { navigate } = this.props.navigation;
-    navigate('Video', { term: card.text })
+    navigate('Video', { term: card.text, uri: card.uri })
   }
   render() {
     return (
@@ -72,7 +227,7 @@ export default class CustomSwipeCards extends React.Component {
         cards={this.state.cards}
         renderCard={(cardData) => <Card onTouchCallback={this.handleCardTouch} {...cardData} />}
         renderNoMoreCards={() => <NoMoreCards />}
-        onClickHandler={this.handleTouch}
+        onClickHandler={this.handleCardTouch}
         handleYup={this.handleYup}
         handleNope={this.handleNope}
         loop
--- VideoView.js
+++ VideoView.js
@@ -13,7 +13,139 @@ import {
 } from 'react-native';
 
 import Video from 'react-native-video';
-
+const aslVideos = {
+"Good afternoon": require('./videos/goodafternoon.mp4'),
+"Good morning": require('./videos/goodmorning.mp4'),
+"Goodbye": require('./videos/bye.mp4'),
+"Goodnight": require('./videos/goodnight.mp4'),
+"Hello": require('./videos/hello.mp4'),
+"How are you?": require('./videos/howareyou.mp4'),
+"How are you? (2)": require('./videos/howareyou2.mp4'),
+"My name is Deborah (name sign).": require('./videos/mynamedeborah.mp4'),
+"Nice to meet you.": require('./videos/nicetomeetyou.mp4'),
+"Thank you.": require('./videos/thankyou.mp4'),
+"Thank you. (2)": require('./videos/thank-you.mp4'),
+"What is your name?": require('./videos/whatyourname.mp4'),
+"You’re welcome.": require('./videos/yourewelcome.mp4'),
+"ASL Alphabet (A-Z)": require('./videos/Unit1_Alphabet.mp4'),
+"BOY": require('./videos/boy.mp4'),
+"DEAF": require('./videos/deaf.mp4'),
+"GIRL": require('./videos/girl.mp4'),
+"HEARING": require('./videos/hearing.mp4'),
+"HOW": require('./videos/how.mp4'),
+"HOW (2)": require('./videos/how2.mp4'),
+"MAN": require('./videos/man.mp4'),
+"MAN (2)": require('./videos/man2.mp4'),
+"PLEASE": require('./videos/please.mp4'),
+"STUDENT": require('./videos/student.mp4'),
+"STUDENT (2)": require('./videos/student2.mp4'),
+"TEACHER": require('./videos/teacher.mp4'),
+"WHAT": require('./videos/what.mp4'),
+"WHEN": require('./videos/when.mp4'),
+"WHERE": require('./videos/where.mp4'),
+"WHO": require('./videos/who.mp4'),
+"WHY": require('./videos/why.mp4'),
+"WOMAN": require('./videos/woman.mp4'),
+"WOMAN (2)": require('./videos/woman2.mp4'),
+"Can you (please) sign slower?": require('./videos/pleasesignslower.mp4'),
+"Can you (please) sign that again?": require('./videos/pleasesignagain.mp4'),
+"Can you fingerspell that again?": require('./videos/fingerspellagain.mp4'),
+"Excuse me": require('./videos/excuseme.mp4'),
+"I don’t understand.": require('./videos/idontunderstand.mp4'),
+"I’ll be right back.": require('./videos/ibrb.mp4'),
+"Sorry.": require('./videos/sorry.mp4'),
+"There is no sign for that, you need to fingerspell it.": require('./videos/nosignforthatmustfingerspell.mp4'),
+"What did you/they say?": require('./videos/whatsay.mp4'),
+"What does that mean?": require('./videos/whatdoesthatmean.mp4'),
+"Write it please.": require('./videos/pleasewrite.mp4'),
+"AGAIN": require('./videos/again.mp4'),
+"APPLE": require('./videos/apple-2.mp4'),
+"APPLE (2)": require('./videos/apple.mp4'),
+"BALL": require('./videos/ball.mp4'),
+"BOOK": require('./videos/book.mp4'),
+"BREAD": require('./videos/bread.mp4'),
+"CANDY": require('./videos/candy.mp4'),
+"COKE (Coca Cola)": require('./videos/coke-2.mp4'),
+"DRAW": require('./videos/draw.mp4'),
+"DRINK": require('./videos/drink.mp4'),
+"EAT": require('./videos/eat.mp4'),
+"FOOD": require('./videos/food.mp4'),
+"GRAPES": require('./videos/grapes.mp4'),
+"ICE CREAM": require('./videos/ice-cream.mp4'),
+"KEY": require('./videos/key.mp4'),
+"LOCK": require('./videos/lock.mp4'),
+"MILK": require('./videos/milk.mp4'),
+"MORE": require('./videos/more.mp4'),
+"Numbers 1-10": require('./videos/Unit2_Numbers_1-10.mp4'),
+"PAPER": require('./videos/paper.mp4'),
+"PIZZA": require('./videos/pizza.mp4'),
+"PIZZA (2)": require('./videos/pizza-2.mp4'),
+"SENTENCE": require('./videos/sentence.mp4'),
+"SIGN": require('./videos/sign.mp4'),
+"SODA-POP": require('./videos/soda-pop.mp4'),
+"WANT": require('./videos/want.mp4'),
+"WANT (2)": require('./videos/want-2.mp4'),
+"WATER": require('./videos/water.mp4'),
+"WRITE": require('./videos/write.mp4'),
+"Are you a student?": require('./videos/areyouastudent.mp4'),
+"Are you deaf?": require('./videos/areyoudeaf.mp4'),
+"Are you learning sign language?": require('./videos/youlearnsign.mp4'),
+"No, I’m not deaf, I’m hearing.": require('./videos/noimnotdeafimhearing.mp4'),
+"What is your teacher’s name?": require('./videos/whatsyourteachersname.mp4'),
+"Where are you learning sign language?": require('./videos/whereyoulearnsign.mp4'),
+"Who is your teacher?": require('./videos/whoisyourteacher.mp4'),
+"Why are you learning sign language?": require('./videos/whyyoulearnsign.mp4'),
+"You sign very fluently.": require('./videos/yousignveryfluent.mp4'),
+"ALGEBRA": require('./videos/algebra.mp4'),
+"ALWAYS": require('./videos/always.mp4'),
+"ART": require('./videos/art.mp4'),
+"ASL": require('./videos/asl.mp4'),
+"BAND": require('./videos/band.mp4'),
+"BIOLOGY": require('./videos/biology.mp4'),
+"CHEMISTRY": require('./videos/chemistry.mp4'),
+"CHOIR": require('./videos/choir.mp4'),
+"COLLEGE": require('./videos/college.mp4'),
+"COOK": require('./videos/cook.mp4'),
+"DRAMA": require('./videos/drama.mp4'),
+"ELEMENTARY": require('./videos/elementary-school.mp4'),
+"ENGLISH": require('./videos/english.mp4'),
+"GET-UP": require('./videos/get-up.mp4'),
+"HEALTH": require('./videos/health.mp4'),
+"HIGH-SCHOOL": require('./videos/highschool.mp4'),
+"HISTORY": require('./videos/history.mp4'),
+"JUNIOR-HIGH-SCHOOL": require('./videos/junior-high-school.mp4'),
+"KNOW": require('./videos/know.mp4'),
+"LEARN": require('./videos/learn.mp4'),
+"MAJOR (in college)": require('./videos/major.mp4'),
+"MARTIAL ARTS": require('./videos/martial-arts.mp4'),
+"MATH": require('./videos/math.mp4'),
+"MIDDLE SCHOOL": require('./videos/middle-school.mp4'),
+"MOON": require('./videos/moon.mp4'),
+"MOST": require('./videos/most.mp4'),
+"NEED": require('./videos/need.mp4'),
+"NEVER": require('./videos/never.mp4'),
+"NO": require('./videos/no.mp4'),
+"PHOTOGRAPHY": require('./videos/photography.mp4'),
+"PHYSICS": require('./videos/physics.mp4'),
+"PLAY": require('./videos/play.mp4'),
+"POETRY": require('./videos/poetry.mp4'),
+"PSYCHOLOGY": require('./videos/psychology.mp4'),
+"SCHOOL": require('./videos/school.mp4'),
+"SCIENCE": require('./videos/science.mp4'),
+"SEASON": require('./videos/season.mp4'),
+"SIGN LANGUAGE": require('./videos/sign-language.mp4'),
+"SKULPTING/CERAMICS": require('./videos/skulpting-ceramics.mp4'),
+"SLEEP": require('./videos/sleep.mp4'),
+"SPORTS": require('./videos/sports.mp4'),
+"SUN": require('./videos/sun.mp4'),
+"THIRSTY": require('./videos/thirsty.mp4'),
+"UNIVERSITY": require('./videos/university.mp4'),
+"WAKE-UP": require('./videos/wake-up.mp4'),
+"WARM": require('./videos/warm.mp4'),
+"WEATHER": require('./videos/weather.mp4'),
+"WORK": require('./videos/work.mp4'),
+"YES": require('./videos/yes.mp4'),
+}
 
 export default class VideoView extends Component {
   static navigationOptions = ({navigation}) => ({
@@ -124,6 +256,7 @@ export default class VideoView extends Component {
   }
 
   renderCustomSkin() {
+    const term = this.props.navigation.state.params.term;
     const flexCompleted = this.getCurrentTimePercentage() * 100;
     const flexRemaining = (1 - this.getCurrentTimePercentage()) * 100;
 
@@ -131,7 +264,7 @@ export default class VideoView extends Component {
       <View style={styles.container}>
         <TouchableOpacity style={styles.fullScreen} onPress={() => {this.setState({paused: !this.state.paused})}}>
           <Video
-            source={require('./small.mp4')}
+            source={aslVideos[term]}
             style={styles.fullScreen}
             rate={this.state.rate}
             paused={this.state.paused}
@@ -172,12 +305,13 @@ export default class VideoView extends Component {
   }
 
   renderNativeSkin() {
+    const term = this.props.navigation.state.params.term;
     const videoStyle = this.state.skin == 'embed' ? styles.nativeVideoControls : styles.fullScreen;
     return (
       <View style={styles.container}>
         <View style={styles.fullScreen}>
           <Video
-            source={require('./small.mp4')}
+            source={aslVideos[term]}
             style={videoStyle}
             rate={this.state.rate}
             paused={this.state.paused}
